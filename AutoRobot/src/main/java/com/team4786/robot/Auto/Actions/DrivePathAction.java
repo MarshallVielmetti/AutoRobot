85f7c30b6fa7bceeacaa7ca54b50e8abb6ddb82e
Marshall Vielmetti <mvielmetti@outlook.com>
2021-11-07 12:38:55 -0600

    Added drive

---
+++ AutoRobot/src/main/java/com/team4786/robot/Auto/Actions/DrivePathAction.java
@@ -0,0 +1,12 @@
+package com.team4786.robot.Auto.Actions;
+
+public class DrivePathAction implements Action {
+    
+    public boolean isFinished() {}
+
+    public void update() {}
+
+    public void done() {}
+
+    public void start() {}
+  }
