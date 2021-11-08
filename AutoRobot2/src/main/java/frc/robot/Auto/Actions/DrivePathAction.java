e5bc0d02c84d8e7056dc2a3362955d677baad864
Marshall Vielmetti <mvielmetti@outlook.com>
2021-11-08 09:47:28 -0600

    Significant progress

--- AutoRobot2/src/main/java/frc/robot/Auto/Actions/DrivePathAction.java
+++ AutoRobot2/src/main/java/frc/robot/Auto/Actions/DrivePathAction.java
@@ -4,7 +4,12 @@ import java.util.Set;
 
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Auto.StartingPosition;
+import frc.robot.Auto.Paths.TrajectoryBase;
 
+/**
+ * Class that includes all of the RAMSETE controller logic to drive a
+ * {@link TrajectoryBase}
+ */
 public class DrivePathAction extends ActionBase {
     @Override
     public Set<Subsystem> getRequirements() {
@@ -12,8 +17,8 @@ public class DrivePathAction extends ActionBase {
         return null;
     }
 
-    public DrivePathAction(StartingPosition startingPosition) {
-        super(startingPosition);
+    public DrivePathAction(TrajectoryBase driveTrajectory, StartingPosition startingPosition) {
+
     }
 
 }
--- AutoRobot2/src/main/java/frc/robot/Auto/Modes/AutoModeBase.java
+++ AutoRobot2/src/main/java/frc/robot/Auto/Modes/AutoModeBase.java
@@ -2,33 +2,52 @@ package frc.robot.Auto.Modes;
 
 import java.util.ArrayList;
 
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
 // import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Auto.StartingPosition;
 import frc.robot.Auto.Actions.ActionBase;
+import frc.robot.Auto.Actions.Waypoints;
+import frc.robot.subsystems.DriveSubsystem;
 
 public abstract class AutoModeBase extends SequentialCommandGroup {
     public StartingPosition m_startingPosition;
 
     public AutoModeBase(StartingPosition startingPosition) {
         this.m_startingPosition = startingPosition;
+        DriveSubsystem.getInstance().setOdometry(getInitialPose());
+
         this.buildAutoMode();
+
     };
 
     @Override
     public void initialize() {
         // TODO
-        // Could potentially do something here
-        // Such as resetting all of the encoders
-        // Otherwise handled in robotInit?
-        // I feel like handling here would be beneficial because it would allow for the
-        // position etc. to be set accurately.
     }
 
     abstract void buildAutoMode();
 
     protected StartingPosition getStartingPosition() {
         return this.m_startingPosition;
+    }
+
+    private Pose2d getInitialPose() {
+        Translation2d initialPosition;
+        switch (this.m_startingPosition) {
+        case LEFT:
+            initialPosition = Waypoints.leftStartingPosition;
+            break;
+        case MIDDLE:
+            initialPosition = Waypoints.middleStartingPosition;
+            break;
+        case RIGHT:
+            initialPosition = Waypoints.rightStartingPosition;
+            break;
+        default:
+            initialPosition = Waypoints.leftStartingPosition; // so I guess by default, it starts here
+        }
     }
 
 }
